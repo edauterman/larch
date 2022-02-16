ac929aef52bccae62b35bc308428610d34821122
Emma Dauterman <evd2014@gmail.com>
2022-02-16 11:42:25 -0800

    restructure

---
+++ zkboo/CMakeFiles/Prover.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/common.cc" "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/CMakeFiles/Prover.dir/common.cc.o"
+  "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/prover.cc" "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/CMakeFiles/Prover.dir/prover.cc.o"
+  "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/verifier.cc" "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/CMakeFiles/Prover.dir/verifier.cc.o"
+  "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/view.cc" "/Users/emmadauterman/Projects/zkboo-r1cs/emp-tool/zkboo/CMakeFiles/Prover.dir/view.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/Cellar/openssl@1.1/1.1.1j/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ zkboo/CMakeFiles/Prover.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prover.dir/prover.cc.o"
+  "CMakeFiles/Prover.dir/view.cc.o"
+  "CMakeFiles/Prover.dir/common.cc.o"
+  "CMakeFiles/Prover.dir/verifier.cc.o"
+  "libProver.pdb"
+  "libProver.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
