83d0f1e41b6c19d6d9cf1398ed84c6c34ec58ff8
Emma Dauterman <evd2014@gmail.com>
2022-03-24 13:57:52 -0700

    basic log compiles

--- log/log.cc
+++ log/log.cc
@@ -4,8 +4,18 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <emp-tool/emp-tool.h>
+#include <openssl/evp.h>
+#include <openssl/ec.h>
+#include <openssl/sha.h>
+#include <openssl/ecdsa.h>
+
 #include "../network/log.grpc.pb.h"
 #include "../network/log.pb.h"
+#include "../agent/params.cc"
+#include "../agent/u2f.h"
+#include "../zkboo/src/proof.h"
+#include "../zkboo/src/verifier.h"
 
 #include "log.h"
 
@@ -18,9 +28,39 @@ using grpc::Status;
 
 //using json = nlohmann::json;
 using namespace std;
+using namespace emp;
 
 LogServer::LogServer() {
-}
+    params = Params_new(P256);
+};
+
+void LogServer::GenerateKeyPair() {
+    EC_KEY *key = EC_KEY_new();
+    pkey = EVP_PKEY_new();
+
+    EC_KEY_set_group(key, params->group);
+    EC_KEY_generate_key(key);
+    //EC_KEY_set_private_key(key, sk_map[string((const char *)key_handle, MAX_KH_SIZE)]);
+    EVP_PKEY_assign_EC_KEY(pkey, key);
+};
+
+void LogServer::VerifyProofAndSign(uint8_t *proof_bytes, uint8_t *challenge, uint8_t *ct, uint8_t *iv_bytes, uint8_t *sig_out, unsigned int *sig_len) {
+    Proof proof;
+    proof.Deserialize(proof_bytes, numRands);
+
+    uint64_t low = *((uint64_t *)iv_bytes);
+    uint64_t high = *(((uint64_t *)iv_bytes) + 1);
+    __m128i iv = makeBlock(low, high);
+           
+    // TODO somehow need to check key_comm matches things? and that ct is correctly in the witness? 
+    bool check = VerifyCtCircuit(proof, iv, m_len, challenge_len);
+
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_create();
+    EVP_MD_CTX_init(mdctx);
+    EVP_SignInit(mdctx, EVP_sha256());
+    EVP_SignUpdate(mdctx, challenge, challenge_len);
+    EVP_SignFinal(mdctx, sig_out, sig_len, pkey);
+};
 
 class LogServiceImpl final : public Log::Service {
     public:
@@ -28,8 +68,21 @@ class LogServiceImpl final : public Log::Service {
 
         LogServiceImpl(LogServer &server) : server(server) {}
 
-        Status SendPublishScores(ServerContext *context, const PublishScoresRequest *req, PublishScoresResponse *resp) override {
-            printf("Received publish scores\n");
+        Status SendReg(ServerContext *context, const RegRequest *req, RegResponse *resp) override {
+            printf("Received registration request\n");
+            return Status::OK;
+        }
+
+        Status SendAuth(ServerContext *context, const AuthRequest *req, AuthResponse *resp) override {
+            printf("Received auth request\n");
+            uint8_t sig[MAX_ECDSA_SIG_SIZE];
+            unsigned int sig_len = 0;
+            string proofStr = req->proof();
+            string challengeStr = req->challenge();
+            string ctStr = req->ct();
+            string ivStr = req->iv();
+            server.VerifyProofAndSign((uint8_t *)req->proof().c_str(), (uint8_t *)req->challenge().c_str(), (uint8_t *)req->ct().c_str(), (uint8_t *)req->iv().c_str(), sig, &sig_len);
+            resp->set_sig(sig, sig_len);
             return Status::OK;
         }
 };
--- log/log.h
+++ log/log.h
@@ -1,11 +1,24 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
+#include <openssl/ec.h>
+#include <openssl/evp.h>
+#include <emp-tool/emp-tool.h>
+
+#include "../agent/params.h"
+
 class LogServer {
     public:
         LogServer();
+        void GenerateKeyPair();
+        void VerifyProofAndSign(uint8_t *proof_bytes, uint8_t *challenge, uint8_t *ct, uint8_t *iv_bytes, uint8_t *sig_out, unsigned int *sig_len);
 
     private:
+        const int numRands = 104116;
+        const int m_len = 256;
+        const int challenge_len = 552;
+        EVP_PKEY *pkey;
+        Params params;
         int port;
 
 
