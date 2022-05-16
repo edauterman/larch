83f0bded233bd2642ad1f2d0f2617beb8924bab7
Emma Dauterman <evd2014@gmail.com>
2022-05-16 22:27:50 +0000

    throughput exp

--- agent/throughput_bench.cc
+++ agent/throughput_bench.cc
@@ -7,10 +7,13 @@
 #include "u2f.h"
 #include "../zkboo/utils/timer.h"
 
-#define NUM_WORKERS 10
+#define NUM_WORKERS 20
 
-void makeReqs(int *auths, std::chrono::high_resolution_clock::time_point start) {
-    Client *c = new Client();
+void init(Client *c) {
+    c->Initialize();
+}
+
+void makeReqs(Client *c, int *auths, std::chrono::high_resolution_clock::time_point start) {
     uint8_t app_id[32];
     uint8_t challenge[32];
     uint8_t key_handle[32];
@@ -18,7 +21,6 @@ void makeReqs(int *auths, std::chrono::high_resolution_clock::time_point start) 
     uint32_t ctr;
     uint8_t sig_out[MAX_ECDSA_SIG_SIZE];
     uint8_t cert_sig[MAX_KH_SIZE + MAX_CERT_SIZE + MAX_ECDSA_SIG_SIZE];
-    c->Initialize();
     fprintf(stderr, "det2f: starting initialize\n");
     //auto start = std::chrono::high_resolution_clock::now();
     //while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() < 60) {
@@ -34,19 +36,46 @@ void makeReqs(int *auths, std::chrono::high_resolution_clock::time_point start) 
     fprintf(stderr, "det2f: finished initialize\n");
 }
 
-int main(int argc, char *argv[]) {
+int experiment(int num_workers) {
     int totalAuths = 0;
-    int auths[NUM_WORKERS];
-    thread workers[NUM_WORKERS];
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < NUM_WORKERS; i++) {
-        auths[i] = 0;
-        workers[i] = thread(makeReqs, &auths[i], start);
+    cout << "workers = " << num_workers << endl;
+    int *auths = (int *)malloc(num_workers * sizeof(int));
+    vector<thread*> workers;
+    vector<Client*> clients;
+    for (int i = 0; i < num_workers; i++) {
+        clients.push_back(new Client());
+        workers.push_back(new thread(init, clients[i]));
     }
-    for (int i = 0; i < NUM_WORKERS; i++) {
-        workers[i].join();
+    for (int i = 0; i < num_workers; i++) {
+        workers[i]->join();
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < num_workers; i++) {
+        auths[i] = 0;
+        workers[i] = new thread(makeReqs, clients[i], &auths[i], start);
+    }
+    for (int i = 0; i < num_workers; i++) {
+        workers[i]->join();
         cout << "individual auths = " << auths[i] << endl;
         totalAuths += auths[i];
     }
     cout << "total auths: " << totalAuths << endl;
+    cout << "throughput : " << (double)totalAuths / 60.0<< endl;
+    return totalAuths;
+}
+
+int main(int argc, char *argv[]) {
+    int workers = 30;
+    int maxAuths = 0;
+    while (true) {
+        int auths = experiment(workers);
+        if (auths < maxAuths) {
+            cout << "MAX AUTHS = " << maxAuths << endl;
+            cout << "workers = " << workers - 5 << endl;
+            cout << "throughput = " << (double)maxAuths / 60.0 << endl;
+            return 0;
+        }
+        maxAuths = auths;
+        workers += 5;
+    }
 }
