c1553a0f96439e77c9d3f17214553a6d4f661586
Emma Dauterman <evd2014@gmail.com>
2022-11-06 06:51:09 +0000

    starting password retrieval with SEAL

---
+++ he/passwords.cpp
@@ -0,0 +1,72 @@
+#include "seal/seal.h"
+
+
+#include "password.h"
+
+using namespace std;
+using namespace seal;
+
+// TODO make PwdClient object with public_key and secret_key and num passwords, n
+
+SEALContext SetContext() {
+    EncryptionParameters params(scheme_type::bfv);
+    size_t poly_modululs_degree = 4096;
+    params.set_poly_modulus_degree(poly_modulus_degree);
+    params.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
+    params.set_plain_modulus(1024);
+    SEALContext context(params);
+    return SEALContext;
+}
+
+PwdClient(SEALContext &context_) {
+    context = context_;
+}
+
+void PwdClient::KeyGen() {
+    KeyGenerator keygen(context);
+    SecretKey secret_key = keygen.secret_key();
+    PublicKey public_key;
+    keygen.create_public_key(public_key);
+}
+
+Ciphertext *PwdClient::GenEncryptedVector(int idx) {
+    Encryptor encryptor(context, public_key);
+    Ciphertext *c = new Ciphertext[num_pwds];
+    for (int i = 0; i < num_pwds; i++) {
+        uint64_t x = i == idx;
+        Plaintext x_plain(uint64_to_hex_string(x));
+        encryptor.encrypt(x_plain, c[i]);
+    }
+    return ciphertext;
+}
+
+uint128_t PwdClient::Decrypt(Ciphertext &c) {
+    Decryptor decryptor(context, secret_key);
+    Plaintext x_dec;
+    decryptor.decrypt(c, x_dec);
+    return x_dec.to_string();   // TODO to int instead of to string
+}
+
+PwdServer(SealContext &context_, PublicKey &public_key_) {
+    context = context_;
+    public_key = public_key_;
+}
+
+Ciphertext PwdServer::Eval(Ciphertext *c, uint128_t *inputs) {
+    Evaluator evaluator(context);
+    Encryptor encryptor(context, public_key);
+    Ciphertext sum;
+    Plaintext zero(uint64_to_hex_string("0"));
+    encryptor.encrypt(zero, sum);
+    for (int i = 0; i < num_pwds; i++) {
+        Plaintext plain(uint64_to_hex_string(input[i]));
+        Ciphertext res;
+        evaluator.multiply_plain(c[i], plain, res);
+        evaluator.add_inplace(sum, res);
+    }
+    return sum;
+}
+
+int main() {
+
+}
