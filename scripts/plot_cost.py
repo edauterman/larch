a0a6a0534eab6130e3813e4d7e573af0b6a996bc
Emma Dauterman <edauterman@berkeley.edu>
2023-04-21 15:17:06 -0700

    analytical plots

---
+++ scripts/plot_cost.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import custom_style
+from custom_style import remove_chart_junk
+import sys
+import numpy as np
+import math
+from math import ceil
+
+max_cpu_hour_cost = 0.085
+network_gb_cost = 0.05
+
+def get_fido2_cost(auths):
+    auth_tput = 6.23
+    auth_sec = auths / auth_tput
+    core_hours = ceil(auth_sec/60.0/60.0)
+    core_cost = max_cpu_hour_cost * core_hours
+
+    data_gb = 352.0 / (1 << 30)
+    network_cost = network_gb_cost * data_gb
+
+    return core_cost + network_cost
+
+def get_pw_cost(auths):
+    auth_tput = 52.63
+    auth_sec = auths / auth_tput
+    core_hours = ceil(auth_sec / 60.0 / 60.0)
+    core_cost = max_cpu_hour_cost * core_hours
+
+    data_gb = 33.0 / (1 << 30)
+    network_cost = network_gb_cost * data_gb
+
+    return core_cost + network_cost
+
+def get_totp_cost(auths):
+    auth_tput = 0.715819
+    auth_sec = auths/auth_tput
+    core_hours = ceil(auth_sec / 60.0 / 60.0)
+    core_cost = max_cpu_hour_cost * core_hours
+
+    data_gb = 36.78454094 / (1 << 20)
+    network_cost = network_gb_cost * data_gb
+
+    return core_cost + network_cost
+
+
+out_name = "plot_cost.pdf" 
+numAuths = [1000 * i for i in range(1,10000)]
+fido2 = []
+pw = []
+totp = []
+colors = [custom_style.mix_colors[3], custom_style.mix_colors[2], custom_style.mix_colors[0]]
+
+
+for i,auths in enumerate(numAuths):
+    pw.append(get_pw_cost(auths))
+    totp.append(get_totp_cost(auths))
+    fido2.append(get_fido2_cost(auths))
+
+fig = plt.figure(figsize = (1.9,1.9))
+ax = fig.add_subplot(111)
+ax.plot(numAuths, pw, label="Passwords", color=colors[0])
+ax.plot(numAuths, totp, label="TOTP", color=colors[1])
+ax.plot(numAuths, fido2, label="FIDO2", color=colors[2])
+#ax.plot(x, yBaseline, label="Plaintext", color="black", linestyle="dashed")
+ax.set_xlabel("Authentications")
+ax.set_ylabel("Cost")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_yticks([0.1, 1,10,100,1000])
+ax.set_yticklabels(["\$0.1","\$1","\$10","\$100","\$1000"])
+ax.set_xticks([1000,10000,100000,1000000,10000000])
+ax.set_xticklabels(["1K","10K","100K","1M","10M"])
+#ax.set_yticks([0, 5, 10, 15, 20])
+#ax.set_xticklabels(["0", "5K", "10K"])
+#ax.set_yticks([0, 1e6, 2e6, 3e6])
+#ax.set_yticklabels(["0", "1M", "2M", "3M"])
+
+handles, labels1 = ax.get_legend_handles_labels()
+handles.reverse()
+labels1.reverse()
+#ax.legend(handles, labels1, fontsize=7, labelspacing=0, ncol=1)
+ax.legend(handles, labels1, bbox_to_anchor=(-0.2, 1.1, 1., .102), loc='lower left', ncol=1, borderaxespad=0., fontsize=7,labelspacing=0)
+
+#ax.spines['left'].set_position("zero")
+#ax.spines['bottom'].set_position("zero")
+
+remove_chart_junk(plt,ax, grid=False, below=False)
+ax.yaxis.grid(which='major', color='0.8', linestyle=':')
+custom_style.save_fig(fig, bbox_inches='tight')
---
+++ scripts/plot_storage.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import custom_style
+from custom_style import remove_chart_junk
+import sys
+import numpy as np
+import math
+
+out_name = "plot_storage.pdf"
+x = []
+y1 = []
+y2 = []
+yLong = []
+yBaseline = []
+labels = ["Authentication records", "Presignatures"]
+colors = [custom_style.mix_colors[3], custom_style.mix_colors[0]]
+
+max_auths = 10000
+
+for i in range(1000, 10000, 100):
+    x.append(i)
+    # ct (32) + sig (64) + timestamp (8)
+    storage1 = (i * (32 + 8 + 64)) / 2**20
+    storage2 = ((max_auths - i) * 6) * 32 / 2**20
+    # long term storage: sk (32) + ctr (4) + auth_pk (33) + comm(32)
+    yLong.append((32 + 4 + 33 + 32) / 2**20)
+    # rpid (2) + timestamp (8)
+    storageBaseline = (i * (2 + 8)) / 2**20
+    y1.append(storage1)
+    y2.append(storage2)
+    yBaseline.append(storageBaseline)
+
+fig = plt.figure(figsize = (1.9,1.9))
+ax = fig.add_subplot(111)
+ax.stackplot(x, y1, y2, labels=labels, colors=colors)
+#ax.plot(x, yBaseline, label="Baseline", color="black")
+#ax.plot(x, yBaseline, label="Plaintext", color="black", linestyle="dashed")
+ax.set_xlabel("Authentications \n FIDO2")
+ax.set_ylabel("Log storage (MiB)")
+ax.set_xticks([0, 5000, 10000])
+#ax.set_yticks([0, 5, 10, 15, 20])
+ax.set_xticklabels(["0", "5K", "10K"])
+#ax.set_yticks([0, 1e6, 2e6, 3e6])
+#ax.set_yticklabels(["0", "1M", "2M", "3M"])
+
+handles, labels1 = ax.get_legend_handles_labels()
+handles.reverse()
+labels1.reverse()
+#ax.legend(handles, labels1, fontsize=7, labelspacing=0, ncol=1)
+ax.legend(handles, labels1, bbox_to_anchor=(-0.2, 1.1, 1., .102), loc='lower left', ncol=1, borderaxespad=0., fontsize=7,labelspacing=0)
+
+ax.spines['left'].set_position("zero")
+ax.spines['bottom'].set_position("zero")
+
+remove_chart_junk(plt,ax, grid=False, below=False)
+ax.yaxis.grid(which='major', color='0.9', linestyle=':')
+plt.save_fig(out_name, bbox_inches='tight')
