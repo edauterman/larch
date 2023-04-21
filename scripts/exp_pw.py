93a4866a41e8969b6ee5252d3a70fee539273166
Emma Dauterman <evd2014@gmail.com>
2023-04-21 07:25:51 +0000

    pw experiment

--- scripts/exp_pw.py
+++ scripts/exp_pw.py
@@ -6,8 +6,8 @@ EC2_FILE = "config/ec2.json"
 MACHINES_FILE = "config/pw_machines.json"
 
 def run_pw_exp():
-    #executeCommand("mkdir pw_exp")
-    #provisionAndSetupAll(EC2_FILE, MACHINES_FILE)
+    executeCommand("mkdir pw_exp")
+    provisionAndSetupAll(EC2_FILE, MACHINES_FILE)
     properties = loadPropertyFile(EC2_FILE)
     machines = loadPropertyFile(MACHINES_FILE)
 
@@ -19,7 +19,7 @@ def run_pw_exp():
     executeRemoteCommand(getHostName(machines['client_ip_address']), 'cd zkboo-r1cs; ./build/bin/pw_latency_bench %s ~/pw_exp/out' % (machines['server_ip_address']), key=properties['secret_key_path']) 
 
     getDirectory('.', [getHostName(machines['client_ip_address'])], '~/pw_exp', key=properties['secret_key_path'])
-    #teardown(EC2_FILE)
+    teardown(EC2_FILE)
 
 run_pw_exp()
 
