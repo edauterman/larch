5525769c9b9c11630e8967ca9f325746f34385e7
Emma Dauterman <edauterman@berkeley.edu>
2023-04-23 21:55:25 -0700

    password benchmark fix

--- client/bench/pw_latency_bench.cc
+++ client/bench/pw_latency_bench.cc
@@ -43,19 +43,17 @@ int main(int argc, char *argv[]) {
 
         // Run authentications
         cout << "Starting authentications for " << lens[i] << endl;
-        uint32_t logMs = 0;
         double clientMs = 0;
         auto t1 = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < rounds; j++) {
             EC_POINT *pw_test = c->Authenticate(to_string(0));
-            logMs += c->GetLogMs();
             clientMs += c->clientMs;
             c->clientMs = 0;
  
         }
         auto t2 = std::chrono::high_resolution_clock::now();
         double totalMs = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / (double)rounds;
-        logMs = (double)logMs / (double)rounds;
+        double logMs = (double)c->GetLogMs() / (double)rounds;
         clientMs = (double)clientMs / (double)rounds;
         cout << "Log ms: " << logMs << endl;
         cout << "Client ms: " << clientMs << endl;
